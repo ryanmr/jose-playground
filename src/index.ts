5cf8edeaadbde6fe06f03e6e5d890c2d1518efce
Ryan Rampersad <ryan.rampersad@gmail.com>
2022-04-16 23:49:04 -0500

    Adds debug, user me endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import * as jose from "jose";
+import { User } from "./types";
 
 const app = express();
+
+// optional: pass in DEBUG=debug [start command]
+const DEBUG = process.env.DEBUG?.includes("debug");
 
 app.get("/", (req, res) => {
   res.json({
@@ -24,7 +28,7 @@ app.use("/", async (req, res, next) => {
 
   if (!authorization || !authorization?.startsWith("Bearer ")) {
     console.warn("🔴 no authorization header");
-    return res.status(403).end();
+    return res.status(401).end();
   }
 
   const jwt = authorization.substring(7, authorization.length);
@@ -36,16 +40,29 @@ app.use("/", async (req, res, next) => {
     });
 
     console.log(`🟢 ${JSON.stringify(payload, null, 2)}`);
+
+    req.user = payload;
   } catch (err: any) {
+    let code = 19000;
     // there may be more errors we should handle
     // https://github.com/panva/jose/tree/main/docs/classes
     if (err?.code === "ERR_JWKS_NO_MATCHING_KEY") {
       console.warn("🟠 no matching key jwks found");
+      code = 19001;
     } else if (err?.code === "ERR_JWT_CLAIM_VALIDATION_FAILED") {
       console.warn(`🟠 ${err?.claim ?? "unknown"} was unexpected`);
+      code = 19002;
+    } else if (err?.code === "ERR_JWT_EXPIRED") {
+      console.warn(`🟠 token expired from ${err?.claim ?? "unknown"}`);
+      code = 19003;
     } else {
       console.log(`🔴`);
       console.error(err);
+      code = 190133;
+    }
+
+    if (DEBUG) {
+      res.json({ code });
     }
 
     return res.status(403).end();
@@ -54,9 +71,19 @@ app.use("/", async (req, res, next) => {
   next();
 });
 
-app.get("/authenticated", (req, res) => {
+app.get("/me", (req, res) => {
+  const user = req.user;
+
+  return res.json({ user });
+});
+
+app.get("/products", (req, res) => {
   res.json({
-    message: "this would be authenticated",
+    products: [
+      { id: 1, name: "cookies" },
+      { id: 2, name: "soup" },
+      { id: 3, name: "eggs" },
+    ],
   });
 });
 
